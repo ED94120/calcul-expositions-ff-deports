8199301f5a83bcfb8e24e1c26a413a115310c24b
ED94120 <denelleeric@gmail.com>
2026-04-19 11:01:43 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+export const DATA_BASE_URL = "https://raw.githubusercontent.com/ED94120/antenna-pattern-tool/main/data";
 
+export const ANTENNA_SPECS_URL = `${DATA_BASE_URL}/antenna-specs.txt`;
+
+export const DEFAULT_ANFR_ATTENUATION_DB = 4;
+export const DEFAULT_VITRAGE_KEY = "simple";
+
+export const DISPLAY_DECIMALS_DB = 2;
+export const DISPLAY_DECIMALS_W = 2;
+export const DISPLAY_DECIMALS_VM = 4;
+export const COPY_DECIMALS_VM = 2;
