7439c48cd5f4616dac1e67d011c40238e8cdfd11
ED94120 <denelleeric@gmail.com>
2026-04-19 10:44:43 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,19 @@
+export const VITRAGE_KEYS = {
+  EXTERIEUR: "exterieur",
+  SIMPLE: "simple",
+  DOUBLE: "double",
+  FAIBLE_EMISSIVITE: "faible_emissivite"
+};
 
+export const VITRAGE_ATT_DB = {
+  [VITRAGE_KEYS.EXTERIEUR]: 0,
+  [VITRAGE_KEYS.SIMPLE]: 2,
+  [VITRAGE_KEYS.DOUBLE]: 3.8,
+  [VITRAGE_KEYS.FAIBLE_EMISSIVITE]: 25
+};
+
+export const BAND_STATUS = {
+  VIDE: "vide",
+  INVALIDE: "invalide",
+  VALIDE: "valide"
+};
